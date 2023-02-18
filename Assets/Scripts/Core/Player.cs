781e0a95d1729d6cfc3f05636c1251b5359ca897
Nicolas Fabrice Baeriswyl <nicolas@baeriswyl.dev>
2023-02-18 11:12:29 +0100

    wip: player initialisation

--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -1,30 +1,36 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using DefaultNamespace;
 using Objects;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Core
 {
     public class Player : MonoBehaviour
     {
-        public int MovementSpeed = 1;
-        public int TurningSpeed = 1;
+        public int movementSpeed = 1;
+        public int turningSpeed = 1;
 
-        public Weapon CurrentWeapon;
-        public float Health;
+        public Weapon currentWeapon;
+        
+        // todo: remove and replace with map
+        public KeyCode keyRight;
+        public KeyCode keyLeft;
+        public KeyCode keyAction1;
+        public KeyCode keyAction2;
 
-        public KeyCode KeyRight;
-        public KeyCode KeyLeft;
-        public KeyCode KeyEvent1;
-        public KeyCode KeyEvent2;
+        private float _health;
 
+        private PlayerData _playerData;
         private Rigidbody2D _rb;
+        private Controller _ctrl;
 
-        private const int MovementSpeedAdjustment = 200;
         private const int TurningSpeedAdjustment = 200;
 
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             _rb = GetComponent<Rigidbody2D>();
         }
@@ -32,26 +38,31 @@ namespace Core
         // Updated 60 times per seconds
         private void FixedUpdate()
         {
-            if (Input.GetKey(KeyRight) && !Input.GetKey(KeyLeft))
+            if (Input.GetKey(keyRight) && !Input.GetKey(keyLeft))
             {
                 // turn right
-                transform.Rotate(Vector3.back * (TurningSpeed * TurningSpeedAdjustment * Time.fixedDeltaTime));
+                transform.Rotate(Vector3.back * (turningSpeed * TurningSpeedAdjustment * Time.fixedDeltaTime));
             }
 
-            if (!Input.GetKey(KeyRight) && Input.GetKey(KeyLeft))
+            if (!Input.GetKey(keyRight) && Input.GetKey(keyLeft))
             {
                 // turn left
-                transform.Rotate(Vector3.forward * (TurningSpeed * TurningSpeedAdjustment * Time.fixedDeltaTime));
+                transform.Rotate(Vector3.forward * (turningSpeed * TurningSpeedAdjustment * Time.fixedDeltaTime));
             }
             
-            if (!(Input.GetKey(KeyRight) && Input.GetKey(KeyLeft)))
+            if (!(Input.GetKey(keyRight) && Input.GetKey(keyLeft)))
             {
                 // go in forward facing direction
-                _rb.velocity = transform.up * (MovementSpeed * Globals.SpeedAdjFactor);
+                _rb.velocity = transform.up * (movementSpeed * Globals.SpeedAdjFactor);
             }
             else
             {
                 _rb.velocity = Vector2.zero;
+            }
+
+            if (_health <= 0)
+            {
+                _ctrl.AnnounceDeath(this);
             }
         }
 
@@ -60,9 +71,41 @@ namespace Core
             var b = col.gameObject.GetComponent<Bullet>();
             if (b != null)
             {
-                Health -= b.GetDamage();
+                _health -= b.GetDamage();
                 Destroy(col.gameObject);
             }
+        }
+
+        public void Initialize(PlayerData p)
+        {
+            keyLeft = p.KeyMap[Globals.KeyFunctions.TurnLeft];
+            keyRight = p.KeyMap[Globals.KeyFunctions.TurnRight];
+            keyAction1 = p.KeyMap[Globals.KeyFunctions.Action1];
+            keyAction2 = p.KeyMap[Globals.KeyFunctions.Action2];
+
+            _health = p.InitialHealth;
+
+            transform.Find(Globals.PlayerHeadSprite).GetComponent<SpriteRenderer>().color = p.Color;
+        }
+
+        public PlayerData GetData()
+        {
+            return _playerData;
+        }
+    }
+
+    public class PlayerData
+    {
+        public readonly Dictionary<string, KeyCode> KeyMap;
+        public string Name;
+        public Color Color;
+        
+        public float InitialHealth = 10;
+
+        public PlayerData(Dictionary<string, KeyCode> keyMap, Color color)
+        {
+            KeyMap = keyMap;
+            Color = color;
         }
     }
 }
